1e7b7ccd1c235d1b925e562719975ef97d8472cd
Aryan Lokhande <lokhandearyan0304@gmail.com>
2025-06-19 21:56:54 +0530

    Initial commit

---
+++ variables.js
@@ -0,0 +1,24 @@
+// Variables
+var number= 1;
+var number1= 2;
+var namePerson="aryan";
+console.log(number + number1);
+console.log(number+" "+ namePerson);
+
+let nameGirl="aditi";
+let no=2;
+let no2=27;
+console.log(nameGirl+ (no+ no2));
+
+let finish;
+finish="bright";
+let color="Red";
+console.log("Finish " + finish + " and " +"Color " + color);
+
+// Constants
+const FIXED= 15;
+// FIXED=1;
+console.log("Constant value: "+FIXED);
+
+
+
